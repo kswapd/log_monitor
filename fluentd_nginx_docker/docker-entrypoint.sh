ab3216fdf8c309e4172cae33c220a197e98c4a53
kongxiangwen <xw.kong@hnair.com>
2016-12-07 20:37:42 +0800

    add nginx dockerfile

--- fluentd_nginx_docker/docker-entrypoint.sh
+++ fluentd_nginx_docker/docker-entrypoint.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+set -e
+
 echo "start nginx..."
 echo "$@"
-nginx -g "daemon on"
-fluentd -d pid.txt -c /fluentd/etc/fluent.conf
+nginx -g "daemon off;" &
+fluentd -c /fluentd/etc/fluent.conf
+#fluentd -d pid.txt -c /fluentd/etc/fluent.conf
 echo "nginx start success"
 #exec "$@"
 
