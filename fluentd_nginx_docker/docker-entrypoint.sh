416d93ceda67547abf01a6321fe6c01221f39e29
kongxiangwen <xw.kong@hnair.com>
2016-12-08 09:50:38 +0000

    add nginx_monitor

--- fluentd_nginx_docker/docker-entrypoint.sh
+++ fluentd_nginx_docker/docker-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
-
 echo "start nginx..."
 echo "$@"
-nginx -g "daemon off;" &
-fluentd -c /fluentd/etc/fluent.conf
-#fluentd -d pid.txt -c /fluentd/etc/fluent.conf
+/usr/local/bin/nginx_monitor -nginx_host=localhost -nginx_port=80 -kafka_topic=capability-nginx -kafka_broker_list=192.168.100.180:8074,192.168.100.181:8074,192.168.100.182:8074 &
+fluentd -c /fluentd/etc/fluent.conf &
+nginx -g "daemon off;"
 echo "nginx start success"
 #exec "$@"
 
